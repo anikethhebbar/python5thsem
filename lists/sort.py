5c2cc7bbbc072a00d803974f366f04fb53d8189d
anikethhebbar <76802363+anikethhebbar@users.noreply.github.com>
2022-11-14 12:50:09 +0530

    added notes and some code

---
+++ lists/sort.py
@@ -0,0 +1,9 @@
+#to sort something reverse in a list
+managers =['carlo_ancelotti','zidane','pep', 'tenhag', 'conte']
+managers.sort(reverse=True)
+print(managers)
+#to sort something in a list
+
+managers1 =['carlo_ancelotti','zidane','pep', 'tenhag', 'conte']
+managers.sort()
+print(managers)
